371cc8bbddd5803033d32707e92d3b807e0a5d19
Diya411 <148410930+Diya411@users.noreply.github.com>
2024-11-19 18:10:08 +0530

    Update day2.c

--- solutions/day2.c
+++ solutions/day2.c
@@ -1,1 +1,17 @@
-
+#include <stdio.h>
+int main() {
+    int N, i, j, spaces;
+    printf("Enter the number of levels: ");
+    scanf("%d", &N);
+    for (i = 1; i <= N; i++) {
+        spaces = N - i;
+        for (j = 1; j <= spaces; j++) {
+            printf(" ");
+        }
+        for (j = 1; j <= (2 * i - 1); j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
