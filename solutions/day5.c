4783946fab5a1bf2f749fa4b1e94dc056555d58a
Diya411 <148410930+Diya411@users.noreply.github.com>
2024-11-23 12:14:14 +0530

    Update day5.c

--- solutions/day5.c
+++ solutions/day5.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main() {
+    int n, sum = 0;
+    printf("Enter no of elements: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    printf("Enter %d elements: ",n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+  
+    for(int i = 0; i < n; i++) {
+        if(arr[i] > 0) {
+            sum += arr[i];
+        }
+    }
+  
+    printf("%d\n", sum);
+    
+    return 0;
+}
